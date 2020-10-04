f1ffaacc5273bb0420f8581188ef7dacc4527a58
mugukamil <mugukamil@gmail.com>
2020-10-04 10:40:08 +0300

    Updates

--- precache-manifest.db610c8faef1eb3cec8abe3d17b45057.js
+++ precache-manifest.db610c8faef1eb3cec8abe3d17b45057.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b6c6fb78248e3276b3265508127f48b",
+    "revision": "7e93544785930e476cc7be8f13d86719",
     "url": "/github.io/contactmanager/index.html"
   },
   {
-    "revision": "28a7f8c8d398225120f0",
+    "revision": "92f5c1b5ec110260b5b7",
     "url": "/github.io/contactmanager/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "3e381e8e8a02723b250a",
+    "revision": "4ccf48f0be1408149559",
     "url": "/github.io/contactmanager/static/css/main.27fd15f6.chunk.css"
   },
   {
-    "revision": "28a7f8c8d398225120f0",
-    "url": "/github.io/contactmanager/static/js/2.385d5b57.chunk.js"
+    "revision": "92f5c1b5ec110260b5b7",
+    "url": "/github.io/contactmanager/static/js/2.dbc2320e.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/github.io/contactmanager/static/js/2.385d5b57.chunk.js.LICENSE.txt"
+    "url": "/github.io/contactmanager/static/js/2.dbc2320e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3e381e8e8a02723b250a",
-    "url": "/github.io/contactmanager/static/js/main.496a3edb.chunk.js"
+    "revision": "4ccf48f0be1408149559",
+    "url": "/github.io/contactmanager/static/js/main.1d1050ef.chunk.js"
   },
   {
     "revision": "43031226ff12498dd9aa",
